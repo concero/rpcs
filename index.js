ba93ba8b47ca403886a13df08f06de3206a55971
Oleg <olegkron.cc@gmail.com>
2025-04-17 22:26:15 +0100

    rm-dist

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.allChains = exports.testnetChains = exports.mainnetChains = exports.supportedChains = void 0;
-const fs_1 = __importDefault(require("fs"));
-const path_1 = __importDefault(require("path"));
-const supportedChainsPath = path_1.default.join(__dirname, "output/supported-chains.json");
-exports.supportedChains = JSON.parse(fs_1.default.readFileSync(supportedChainsPath, "utf-8"));
+var fs = require("fs");
+var path = require("path");
+var supportedChainsPath = path.join(__dirname, "output/supported-chains.json");
+exports.supportedChains = JSON.parse(fs.readFileSync(supportedChainsPath, "utf-8"));
 function loadChainData() {
-    const mainnetChains = {};
-    const testnetChains = {};
-    const allChains = {};
-    Object.entries(exports.supportedChains.mainnet).forEach(([chainId, name]) => {
-        const filePath = path_1.default.join(__dirname, `output/mainnet/${chainId}-${name}.json`);
-        if (fs_1.default.existsSync(filePath)) {
-            const chainData = JSON.parse(fs_1.default.readFileSync(filePath, "utf-8"));
-            mainnetChains[chainId] = chainData;
-            allChains[chainId] = chainData;
+    var mainnetChains = {};
+    var testnetChains = {};
+    var allChains = {};
+    Object.entries(exports.supportedChains.mainnet).forEach(function (_a) {
+        var chainId = _a[0], name = _a[1];
+        var filePath = path.join(__dirname, "output/mainnet/".concat(chainId, "-").concat(name, ".json"));
+        if (fs.existsSync(filePath)) {
+            var chainData_1 = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+            mainnetChains[chainId] = chainData_1;
+            allChains[chainId] = chainData_1;
         }
     });
-    Object.entries(exports.supportedChains.testnet).forEach(([chainId, name]) => {
-        const filePath = path_1.default.join(__dirname, `output/testnet/${chainId}-${name}.json`);
-        if (fs_1.default.existsSync(filePath)) {
-            const chainData = JSON.parse(fs_1.default.readFileSync(filePath, "utf-8"));
-            testnetChains[chainId] = chainData;
-            allChains[chainId] = chainData;
+    Object.entries(exports.supportedChains.testnet).forEach(function (_a) {
+        var chainId = _a[0], name = _a[1];
+        var filePath = path.join(__dirname, "output/testnet/".concat(chainId, "-").concat(name, ".json"));
+        if (fs.existsSync(filePath)) {
+            var chainData_2 = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+            testnetChains[chainId] = chainData_2;
+            allChains[chainId] = chainData_2;
         }
     });
     return {
@@ -34,7 +33,7 @@ function loadChainData() {
         all: allChains,
     };
 }
-const chainData = loadChainData();
+var chainData = loadChainData();
 exports.mainnetChains = chainData.mainnet;
 exports.testnetChains = chainData.testnet;
 exports.allChains = chainData.all;
--- src/utils/sanitizeUrl.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sanitizeUrl = sanitizeUrl;
-function sanitizeUrl(url) {
-    return url
-        .replace(/\/$/, "") // Remove trailing slash
-        .trim(); // Trim whitespace
-}
