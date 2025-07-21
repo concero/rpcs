5b00b4725a71ac393b3d8e7698bf1d22f07c750e
RPC Service <rpcs@concero.io>
2025-07-21 12:32:09 +0100

    build

--- index.js
+++ index.js
@@ -11,29 +11,15 @@ function loadChainData() {
     if (fs.existsSync(mainnetPath)) {
         var mainnetData = JSON.parse(fs.readFileSync(mainnetPath, "utf-8"));
         Object.entries(mainnetData).forEach(function (_a) {
-            var chainId = _a[0], data = _a[1];
-            var chainData = data;
-            var fullChainData = {
-                id: chainId,
-                urls: chainData.urls,
-                chainSelector: chainData.chainSelector,
-                name: chainData.name,
-            };
-            mainnetChains[chainId] = fullChainData;
+            var chainKey = _a[0], data = _a[1];
+            mainnetChains[chainKey] = data;
         });
     }
     if (fs.existsSync(testnetPath)) {
         var testnetData = JSON.parse(fs.readFileSync(testnetPath, "utf-8"));
         Object.entries(testnetData).forEach(function (_a) {
-            var chainId = _a[0], data = _a[1];
-            var chainData = data;
-            var fullChainData = {
-                id: chainId,
-                urls: chainData.urls,
-                chainSelector: chainData.chainSelector,
-                name: chainData.name,
-            };
-            testnetChains[chainId] = fullChainData;
+            var chainKey = _a[0], data = _a[1];
+            testnetChains[chainKey] = data;
         });
     }
     return {
