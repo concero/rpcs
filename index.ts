d5cc6f543591d3d772cb88d67b1caa9d35603ad4
RPC Service <rpcs@concero.io>
2025-07-21 12:31:06 +0100

    upd

--- index.ts
+++ index.ts
@@ -2,10 +2,9 @@ import * as fs from "fs";
 import * as path from "path";
 
 export interface ChainRpcData {
-  id: string;
-  urls: string[];
+  rpcUrls: string[];
   chainSelector: number;
-  name: string;
+  chainId: string;
 }
 
 function loadChainData(): {
@@ -20,29 +19,15 @@ function loadChainData(): {
 
   if (fs.existsSync(mainnetPath)) {
     const mainnetData = JSON.parse(fs.readFileSync(mainnetPath, "utf-8"));
-    Object.entries(mainnetData).forEach(([chainId, data]) => {
-      const chainData = data as Omit<ChainRpcData, "id">;
-      const fullChainData: ChainRpcData = {
-        id: chainId,
-        urls: chainData.urls,
-        chainSelector: chainData.chainSelector,
-        name: chainData.name,
-      };
-      mainnetChains[chainId] = fullChainData;
+    Object.entries(mainnetData).forEach(([chainKey, data]) => {
+      mainnetChains[chainKey] = data as ChainRpcData;
     });
   }
 
   if (fs.existsSync(testnetPath)) {
     const testnetData = JSON.parse(fs.readFileSync(testnetPath, "utf-8"));
-    Object.entries(testnetData).forEach(([chainId, data]) => {
-      const chainData = data as Omit<ChainRpcData, "id">;
-      const fullChainData: ChainRpcData = {
-        id: chainId,
-        urls: chainData.urls,
-        chainSelector: chainData.chainSelector,
-        name: chainData.name,
-      };
-      testnetChains[chainId] = fullChainData;
+    Object.entries(testnetData).forEach(([chainKey, data]) => {
+      testnetChains[chainKey] = data as ChainRpcData;
     });
   }
 
