9558e5198bb4c2fb5227c307da179cdc5c8ca7d7
RPC Service <rpcs@concero.io>
2025-07-21 13:01:36 +0100

    upd types

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,7 @@
 export interface ChainRpcData {
-    id: string;
-    urls: string[];
+    rpcUrls: string[];
     chainSelector: number;
-    name: string;
+    chainId: string;
 }
-export interface SupportedChains {
-    mainnet: Record<string, string>;
-    testnet: Record<string, string>;
-}
-export declare const supportedChains: SupportedChains;
 export declare const mainnetChains: Record<string, ChainRpcData>;
 export declare const testnetChains: Record<string, ChainRpcData>;
-export declare const allChains: Record<string, ChainRpcData>;
